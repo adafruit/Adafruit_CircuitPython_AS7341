0ea701dc5cb131c0328367216313e044c7069c4d
siddacious <nospam187+github@gmail.com>
2020-07-09 16:21:29 -0700

    fixing dox

--- adafruit_as7341.py
+++ adafruit_as7341.py
@@ -184,14 +184,10 @@ class AS7341:  # pylint:disable=too-many-instance-attributes
     _cfg0 = UnaryStruct(_AS7341_CFG0, "<B")
     atime = UnaryStruct(_AS7341_ATIME, "<B")
     """The integration time step count.
-    Total integration time will be `(ATIME + 1) * (ASTEP + 1) * 2.78µS` *
+    Total integration time will be ``(ATIME + 1) * (ASTEP + 1) * 2.78µS``
     """
     astep = UnaryStruct(_AS7341_ASTEP_L, "<H")
-    """ The integration time step size
-    *
-    * @param astep_value Integration time step size in 2.78 microsecon increments
-    * Step size is `(astep_value+1) * 2.78 uS`
-    """
+    """ The integration time step size in 2.78 microsecond increments"""
     _gain = UnaryStruct(_AS7341_CFG1, "<B")
     """The ADC gain multiplier
     *
@@ -297,11 +293,7 @@ class AS7341:  # pylint:disable=too-many-instance-attributes
     # F8 680 52
     @property
     def gain(self):
-        """The ADC gain multiplier
-        *
-        * @param gain_value The gain amount. must be an `as7341_gain_t`
-        * @return true: success false: failure
-        */
+        """The ADC gain multiplier. Must be a valid `adafruit_as7341.Gain`
         """
         return self._gain
 
@@ -353,112 +345,6 @@ class AS7341:  # pylint:disable=too-many-instance-attributes
         self._led_enabled = led_on
 
 
-# #ifndef _ADAFRUIT_AS7341_H
-# __ADAFRUIT_AS7341_H = const(
-# ) # #include "Arduino.h"
-# #include <Adafruit_BusIO_Register.h>
-# #include <Adafruit_I2CDevice.h>
-# #include <Adafruit_Sensor.h>
-# #include <Wire.h>
-# _AS7341_I2CADDR_DEFAULT = const(0x39) # AS7341 default i2c address
-# _AS7341_CHIP_ID = const(0x09) # AS7341 default device id from WHOAMI
-
-# _AS7341_WHOAMI = const(0x92) # Chip ID register
-
-
-# _AS7341_CONFIG = const(0x70) # Enables LED control and sets light sensing mode
-
-
-# _AS7341_GPIO = const(0x73) # Connects photo diode to GPIO or INT pins
-# _AS7341_LED = const(0x74) # LED Register; Enables and sets current limit
-# _AS7341_ENABLE = const(                                                         \) #
-#   0x80 Main enable register. Controls SMUX, Flicker Detection, Spectral
-# Measurements and Power
-# _AS7341_ATIME = const(0x81) # Sets ADC integration step count
-
-# _AS7341_SP_LOW_TH_L = const(0x84) # Spectral measurement Low Threshold low byte
-# _AS7341_SP_LOW_TH_H = const(                                                    \) #
-#   0x85 Spectral measurement Low Threshold high byte
-# _AS7341_SP_HIGH_TH_L = const(                                                   \) #
-#   0x86 Spectral measurement High Threshold low byte
-# _AS7341_SP_HIGH_TH_H = const(                                                   \) #
-#   0x87 Spectral measurement High Threshold low byte
-
-
-# _AS7341_STATUS = const(                                                         \) #
-#   0x93 Interrupt status registers. Indicates the occourance of an interrupt
-
-# _AS7341_CH0_DATA_L = const(0x95) # ADC Channel Data
-# _AS7341_CH0_DATA_H = const(0x96) # ADC Channel Data
-# _AS7341_CH1_DATA_L = const(0x97) # ADC Channel Data
-# _AS7341_CH1_DATA_H = const(0x98) # ADC Channel Data
-# _AS7341_CH2_DATA_L = const(0x99) # ADC Channel Data
-# _AS7341_CH2_DATA_H = const(0x9A) # ADC Channel Data
-# _AS7341_CH3_DATA_L = const(0x9B) # ADC Channel Data
-# _AS7341_CH3_DATA_H = const(0x9C) # ADC Channel Data
-# _AS7341_CH4_DATA_L = const(0x9D) # ADC Channel Data
-# _AS7341_CH4_DATA_H = const(0x9E) # ADC Channel Data
-# _AS7341_CH5_DATA_L = const(0x9F) # ADC Channel Data
-# _AS7341_CH5_DATA_H = const(0xA0) # ADC Channel Data
-# _AS7341_STATUS2 = const(0xA3) # Measurement status flags; saturation, validity
-# _AS7341_STATUS3 = const(                                                        \) #
-#   0xA4 Spectral interrupt source, high or low threshold
-
-
-# _AS7341_CFG0 = const(                                                           \) #
-#   0xA9 Sets Low power mode, Register bank, and Trigger lengthening
-# _AS7341_CFG1 = const(0xAA) # Controls ADC Gain
-
-
-# _AS7341_CFG9 = const(                                                           \) #
-#   0xB2 Enables flicker detection and smux command completion system
-# interrupts
-
-# _AS7341_CFG12 = const(                                                          \) #
-#   0xB5 Spectral threshold channel for interrupts, persistence and auto-gain
-# _AS7341_PERS = const(                                                           \) #
-#   0xBD Number of measurement cycles outside thresholds to trigger an
-# interupt
-# _AS7341_GPIO2 = const(                                                          \) #
-#   0xBE GPIO Settings and status: polarity, direction, sets output, reads
-# input
-# _AS7341_ASTEP_L = const(0xCA) # Integration step size ow byte
-# _AS7341_ASTEP_H = const(0xCB) # Integration step size high byte
-
-
-# _AS7341_FD_TIME1 = const(0xD8) # Flicker detection integration time low byte
-# _AS7341_FD_TIME2 = const(0xDA) # Flicker detection gain and high nibble
-
-# _AS7341_FD_STATUS = const(                                                      \) #
-#   0xDB Flicker detection status; measurement valid, saturation, flicker
-# type
-# _AS7341_INTENAB = const(0xF9) # Enables individual interrupt types
-# _AS7341_CONTROL = const(0xFA) # Auto-zero, fifo clear, clear SAI active
-
-
-# _AS7341_SPECTRAL_INT_HIGH_MSK = const(                                          \) #
-#   0b00100000 bitmask to check for a high threshold interrupt
-# _AS7341_SPECTRAL_INT_LOW_MSK = const(                                           \) #
-#   0b00010000 bitmask to check for a low threshold interrupt
-
-# /**
-#  * @brief Allowable gain multipliers for `setGain`
-#  *
-#  */
-# typedef enum {
-#   AS7341_GAIN_0_5X,
-#   AS7341_GAIN_1X,
-#   AS7341_GAIN_2X,
-#   AS7341_GAIN_4X,
-#   AS7341_GAIN_8X,
-#   AS7341_GAIN_16X,
-#   AS7341_GAIN_32X,
-#   AS7341_GAIN_64X,
-#   AS7341_GAIN_128X,
-#   AS7341_GAIN_256X,
-#   AS7341_GAIN_512X,
-# } as7341_gain_t;
-
 # /**
 #  * @brief ADC Channel specifiers for configuration
 #  *
@@ -495,80 +381,3 @@ class AS7341:  # pylint:disable=too-many-instance-attributes
 #   AS7341_INT_COUNT_55, 14
 #   AS7341_INT_COUNT_60, 15
 # } as7341_int_cycle_count_t;
-
-# class Adafruit_AS7341;
-
-# /*!
-#  *    @brief  Class that stores state and functions for interacting with
-#  *            the AS7341 11-Channel Spectral Sensor
-#  */
-# class Adafruit_AS7341 {
-# public:
-#   Adafruit_AS7341();
-#   ~Adafruit_AS7341();
-
-#   bool begin(uint8_t i2c_addr = AS7341_I2CADDR_DEFAULT, TwoWire *wire = &Wire,
-#              int32_t sensor_id = 0);
-
-#   bool setASTEP(uint16_t astep_value);
-#   bool setATIME(uint8_t atime_value);
-#   bool setGain(as7341_gain_t gain_value);
-
-#   uint16_t readChannel(as7341_channel_t channel);
-
-#   void readRawValuesMode1(void);
-#   void readRawValuesMode2(void);
-
-#   void flickerDetection(void); // merge with 1k
-#   void flickerDetection1K(void);
-
-#   void FDConfig(void);
-
-#   int8_t getFlickerDetectStatus(void);
-
-#   void F1F4_Clear_NIR(void);
-#   void F5F8_Clear_NIR(void);
-
-#   void powerEnable(bool enable_power);
-#   bool enableSpectralMeasurement(bool enable_measurement);
-
-#   bool setHighThreshold(int16_t high_threshold);
-#   bool setLowThreshold(int16_t low_threshold);
-
-#   int16_t getHighThreshold(void);
-#   int16_t getLowThreshold(void);
-
-#   bool enableSpectralINT(bool enable_int);
-#   bool setAPERS(as7341_int_cycle_count_t cycle_count);
-#   bool setSpectralThresholdChannel(as7341_channel_t channel);
-
-#   uint8_t getInterruptStatus(void);
-#   bool clearInterruptStatus(void);
-
-#   bool spectralInterruptTriggered(void);
-#   uint8_t spectralINTSource(void);
-#   bool spectralLowTriggered(void);
-#   bool spectralHighTriggered(void);
-
-#   bool enableLED(bool enable_led);
-
-#   bool setLEDCurrent(uint8_t led_current);
-
-#   bool getIsDataReady();
-#   bool setBank(bool low); // low true gives access to 0x60 to 0x74
-
-# protected:
-#   virtual bool _init(int32_t sensor_id);
-#   uint8_t last_spectral_int_source =
-#       0; The value of the last reading of the spectral interrupt source
-# register
-
-#   Adafruit_I2CDevice *i2c_dev = NULL; Pointer to I2C bus interface
-
-# private:
-#   void enableSMUX(void);
-#   void SmuxConfigRAM(void);
-#   void writeRegister(byte addr, byte val);
-# };
-
-# #endif
