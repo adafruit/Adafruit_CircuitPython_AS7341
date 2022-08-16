d8f42a854a17a269b6002102fc523192312f7cd1
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:12 -0400

    Update version string

--- adafruit_as7341.py
+++ adafruit_as7341.py
@@ -32,7 +32,7 @@ Implementation Notes
 
 """
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_AS7341.git"
 
 from time import sleep, monotonic
