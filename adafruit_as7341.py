0afda19db2d26a74f38e2e726759e526cb0717b9
foamyguy <foamyguy@gmail.com>
2023-04-17 16:21:42 -0500

    catch return from all_channels

--- adafruit_as7341.py
+++ adafruit_as7341.py
@@ -436,13 +436,13 @@ class AS7341:  # pylint:disable=too-many-instance-attributes, no-member
     @property
     def channel_clear(self) -> int:
         """The current reading for the clear sensor"""
-        self._all_channels
+        _ = self._all_channels
         return self._channel_4_data
 
     @property
     def channel_nir(self) -> int:
         """The current reading for the NIR (near-IR) sensor"""
-        self._all_channels
+        _ = self._all_channels
         return self._channel_5_data
 
     def _wait_for_data(self, timeout: float = 1.0) -> None:
@@ -465,7 +465,7 @@ class AS7341:  # pylint:disable=too-many-instance-attributes, no-member
         """Configure the sensor to read from elements F1-F4, Clear, and NIR"""
         # disable SP_EN bit while  making config changes
         if self._low_channels_configured:
-            self._all_channels
+            _ = self._all_channels
             return
         self._high_channels_configured = False
         self._flicker_detection_1k_configured = False
@@ -489,7 +489,7 @@ class AS7341:  # pylint:disable=too-many-instance-attributes, no-member
         """Configure the sensor to read from elements F5-F8, Clear, and NIR"""
         # disable SP_EN bit while  making config changes
         if self._high_channels_configured:
-            self._all_channels
+            _ = self._all_channels
             return
 
         self._low_channels_configured = False
